c5aed1ffe9eec31994c681435cbbd64cf52ecc2d
arshiam3 <arshiam3@nycstudents.net>
2026-05-20 09:56:09 -0400

    Update Bottle.java

--- question-2/Bottle.java
+++ question-2/Bottle.java
@@ -1,1 +1,26 @@
+public class Bottle {
+    // instance variables
+    private double capacity;
 
+    public Bottle(double maxCap) {
+        capacity = maxCap;
+
+
+    }
+
+    public double updateAmount(double removed) {
+        double currAmount = 0.0;
+        currAmount = capacity - removed;
+        if (currAmount == capacity*0.25) {
+            currAmount = capacity;
+        }
+
+        return currAmount;
+
+
+
+    }
+
+
+
+}
