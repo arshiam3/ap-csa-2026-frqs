0edf7c768a45933935a0de5d7ec9012c894290e9
arshiam3 <arshiam3@nycstudents.net>
2026-05-22 10:01:50 -0400

    Update partA.java

--- question-1/partA.java
+++ question-1/partA.java
@@ -1,1 +1,14 @@
+public Account(String requestedName) {
+            username = requestedName;
+  
+        if (!username.isAvailable()) {
+            int i = 1;
+            while (!username.isAvailable()) {
+                username = username+i;
+                i++;
+            }
+
+        }
+
+    } // end of Account constructor
 
