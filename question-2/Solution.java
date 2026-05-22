7ce77c0bca1745faf09c86f22473be1c93dc087f
arshiam3 <arshiam3@nycstudents.net>
2026-05-22 10:12:50 -0400

    Update Solution.java

--- question-2/Solution.java
+++ question-2/Solution.java
@@ -1,25 +1,31 @@
 public class Bottle {
     // instance variables
     private double capacity;
+    private double currAmt;
 
     public Bottle(double maxCap) {
         capacity = maxCap;
+        currAmt = maxCap;
 
 
     }
 
     public double updateAmount(double removed) {
-        double currAmount = 0.0;
-        currAmount = capacity - removed;
-        if (currAmount < capacity*0.25) {
-            currAmount = capacity;
+        currAmt = currAmt - removed;
+        
+        if (currAmt < capacity*0.25) {
+            currAmt = capacity;
         }
 
-        return currAmount;
+        return currAmt;
 
 
 
     }
+
+
+
+
 
 
 
