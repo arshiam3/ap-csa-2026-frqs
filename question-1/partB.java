573a702098d5dc109bf6e64f7d9b8e5ac8fdcad2
arshiam3 <arshiam3@nycstudents.net>
2026-05-25 22:55:12 -0400

    Update partB.java

--- question-1/partB.java
+++ question-1/partB.java
@@ -1,1 +1,14 @@
+public String getShortenedName() {
+  String shortName = username;
+  while(shortName.indexOf("-") > -1) {
+  shortName = shortName.substring(0, shortName.indexOf("-")-1) + shortName.substring(shortName.indexOf("-")+1
 
+  }
+  return shortName;
+
+
+
+
+
+
+}
