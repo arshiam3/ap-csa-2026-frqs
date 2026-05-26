a2a4d08cc8124391da0c6004ca3c1e210fa558c9
arshiam3 <arshiam3@nycstudents.net>
2026-05-25 23:13:14 -0400

    Update Solution.java

--- question-3/Solution.java
+++ question-3/Solution.java
@@ -1,1 +1,17 @@
+public int moreHistoryThanMathAbsences() { 
+    int count;
+    for (int i = 0; i < historyList.size(); i++) {
+        int numAbsencesH = historyList.get(i).getAbsences(); 
+        String id = historyList.get(i).getStudentID(); 
 
+        for(int j = 0; j < mathList.size(); j++) {
+            int numAbsencesM = mathList.get(j).getAbsences(); 
+            String idTwo = mathList.get(j).getStudentID();
+            if ((idTwo.equals(id)) && (numAbsencesH > numAbsencesM)) {
+                count++; 
+            }
+
+        }       
+    }
+    return count;
+}
