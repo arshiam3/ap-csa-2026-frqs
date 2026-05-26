dd48d20fb4b499fd87ad0631cf8f249e9441e95a
arshiam3 <arshiam3@nycstudents.net>
2026-05-25 23:39:46 -0400

    Update Solution.java

--- question-4/Solution.java
+++ question-4/Solution.java
@@ -1,1 +1,15 @@
+public int getPointsForRow(int targetRow) {
+  int numPoints = 0;
+  
+  for (int c = 0; c < board[0].length; c++) {
+    numPoints += board[targetRow][c].getPoints();
+    if (board[targetRow][c].getColor().equals(board[targetRow][0].getColor()) {
+      numPoints *= 2;
+    }
+  }
+return numPoints;
 
+
+
+
+  }
